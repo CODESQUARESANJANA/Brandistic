0910016ce29cde61e541e7b0fceafd26073352ca
ISHIKA-CODESQUARE <ishika.joshi@codesquaretech.com>
2022-07-15 12:31:10 +0530

    hover issue fix

--- responsiv-sidenav.js
+++ responsiv-sidenav.js
@@ -1,14 +1,1 @@
 
-function showdrpdn(){
-    var x=document.getElementById("drpdn-menu")
-    x.style.display="block";
-    console.log("hi")
-    
-}
-
-document.addEventListener("click" , onclck())
-function onclck(){
-    var y=document.getElementById("drpdn-menu")
-    y.style.display="none";
-    console.log("faf");
-}
